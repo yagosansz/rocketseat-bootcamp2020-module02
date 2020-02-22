c10539e80f2688bd88b094557de8498c4d5f23a9
Yago Santos de Sousa <yagosansz@gmail.com>
2020-02-21 20:46:24 -0500

    Added UserController

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Yago Santos',
-    email: 'yago@me.com',
-    password_hash: '12312312'
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
