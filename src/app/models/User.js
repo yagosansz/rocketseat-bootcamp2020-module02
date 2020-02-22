5a0ae4844d162f9aad040935437115d09a2dea7c
Yago Santos de Sousa <yagosansz@gmail.com>
2020-02-21 21:33:41 -0500

    Added encryption to password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 import Sequelize, { Model } from 'sequelize';
 
+import bcrypt from 'bcryptjs';
+
 class User extends Model {
   // sequelize <--> this.connection from database/index.js
   static init(sequelize) {
@@ -7,6 +9,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // will not be stored in the db
         password_hash: Sequelize.STRING,
         provider: Sequelize.STRING
       },
@@ -14,6 +17,17 @@ class User extends Model {
         sequelize // we need to pass the connection instance
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      // if new user is being created or password is being edited
+      if (user.password) {
+        // second argument represents how 'strong' the cryptography will be
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    // returns the model that was intialized
+    return this;
   }
 }
 
