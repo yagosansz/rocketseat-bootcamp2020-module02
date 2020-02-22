89d7bf66424e757e0610907a050be733692d037c
Yago Santos de Sousa <yagosansz@gmail.com>
2020-02-22 11:26:26 -0500

    Added jwt authentication to sessions

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -29,6 +29,10 @@ class User extends Model {
     // returns the model that was intialized
     return this;
   }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
 }
 
 export default User;
