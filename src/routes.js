b24ccacbe9ec1118d573d7c9c53ec5a04dcb510f
Yago Santos de Sousa <yagosansz@gmail.com>
2020-02-28 10:29:19 -0500

    Notifications can be marked as read

--- src/routes.js
+++ src/routes.js
@@ -30,6 +30,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 // 'file' is the field in the request
 routes.post('/files', upload.single('file'), FileController.store);
