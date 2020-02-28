0e5c3e9a83b9278c297f4441bd6e25497f5c30e5
Yago Santos de Sousa <yagosansz@gmail.com>
2020-02-28 11:22:29 -0500

    Added functionality to cancel appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import { Op } from 'sequelize';
 
 import Appointment from '../models/Appointment';
@@ -107,6 +107,32 @@ class AppointmentController {
       content: `${user.name} has an appointment ${formattedDate}`,
       user: provider_id
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    // Check if logged in user is the same one that created the appointment
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'You do not have permission to cancel this appointment'
+      });
+    }
+
+    // subtract 2 hours from appointment date
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments up to 2 hours in advance'
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
